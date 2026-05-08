b92f1b6ebcb4f1a96c711af7a45d230ec6b61a9d
MdAkramKhanJehad <mkhan04@wm.edu>
2026-05-08 12:53:45 -0400

    codeql man val

--- codeql/automated_labelling_basic.py
+++ codeql/automated_labelling_basic.py
@@ -6,8 +6,8 @@ import os
 
 
 rule_id = "java/potentially-weak-cryptographic-algorithm"
-msg_substr = "Cryptographic algorithm [ChaCha20](1) may not be secure"
-matching_substr = '"ChaCha20"'
+msg_substr = "Cryptographic algorithm [ChaCha20-Poly1305](1) may not be secure"
+matching_substr = '"ChaCha20-Poly1305"'
 validation_status = True  
 search_dir = "codeql/preprocessed-output"
 
