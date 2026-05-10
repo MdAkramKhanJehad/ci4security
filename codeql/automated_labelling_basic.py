8e49d4f4f8e949c5a987f2e8f728ce31877f3ff2
MdAkramKhanJehad <mkhan04@wm.edu>
2026-05-10 00:10:40 -0400

    codeql man val

--- codeql/automated_labelling_basic.py
+++ codeql/automated_labelling_basic.py
@@ -5,9 +5,9 @@ import glob
 import os
 
 
-rule_id = "java/weak-cryptographic-algorithm"
-msg_substr = "Cryptographic algorithm [AES/CBC/PKCS7Padding](1) is insecure"
-matching_substr = '"AES/CBC/PKCS7Padding"'
+rule_id = "java/potentially-weak-cryptographic-algorithm"
+msg_substr = "Cryptographic algorithm [AESWrap](1) may not be secure"
+matching_substr = '"AESWrap"'
 validation_status = True  
 search_dir = "codeql/preprocessed-output"
 
